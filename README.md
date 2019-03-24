f0afff762858416acebc3ab70e7bece1c0f64ad8
ukxam <uk_xam@yahoo.com>
2019-03-24 13:53:53 +0530

    refactored commit - with code

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # webcrawler
-Take Home project
+webcrawler on static internet files...
+
+a web crawler is a software that requests pages from the
+Internet, parses the content to extract all the links in the page, and visits the
+links to crawl those pages, to an infinite depth.
+
+Expected output for internet_1.json
+Success:
+[
+"http://foo.bar.com/p1","http://foo.bar.com/p2",
+"http://foo.bar.com/p4","http://foo.bar.com/p5",
+"http://foo.bar.com/p6"
+]
+Skipped:
+[
+"http://foo.bar.com/p1","http://foo.bar.com/p2",
+"http://foo.bar.com/p4","http://foo.bar.com/p5"
+]
+Error:
+[
+"http://foo.bar.com/p3","http://foo.bar.com/p7"
+]
+Expected output: for internet_2.json
+Success:
+[
+"http://foo.bar.com/p1","http://foo.bar.com/p2",
+"http://foo.bar.com/p3","http://foo.bar.com/p4",
+"http://foo.bar.com/p5"
+]
+Skipped:
+[
+"http://foo.bar.com/p1"
+]
+Error:
+[]
+Expected output: for internet_3.json
+Success:
+[
+"http://foo.bar.com/p1"
+]
+Skipped:
+[]
+Error:
+[
+"http://foo.bar.com/p999999999"
+]
--- src/main/java/com/ge/app/webcrawler/App.java
+++ src/main/java/com/ge/app/webcrawler/App.java
@@ -6,10 +6,14 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.json.JSONException;
+import org.json.simple.parser.ParseException;
 
 import com.ge.app.webcrawler.model.WebPage;
 import com.ge.app.webcrawler.processor.WebPageProcessor;
@@ -21,26 +25,31 @@ import com.ge.app.webcrawler.util.ParseUtils;
  *
  */
 public class App {
-	public static void main(String[] args) throws InterruptedException {
-		String inputFile = "input.txt";
+	
+	static String INPUT_FILE = "input.txt";
+	
+	public static void main(String[] args) throws InterruptedException, IOException{
+
 		List<String> fileNames;
-		try (Stream<String> stream = Files.lines(Paths.get(inputFile))) {
+		try (Stream<String> stream = Files.lines(Paths.get(INPUT_FILE))) {
 			fileNames = stream.collect(Collectors.toList());
 		} catch (IOException e) {
-			System.out.println("Invalid or no input provided !!! ");
-			return;
+			String msg = "Invalid or no input provided !!! ";
+			System.out.println(msg);
+			throw e;
 		}
+		
 		if (fileNames.isEmpty()) {
-			System.out.println("No locations present in the input file !!! ");
-			return;
+			String msg = "No locations present in the input file !!! ";
+			System.out.println(msg);
+			throw new RuntimeException(msg);
 		}
 
 		ExecutorService executorService = Executors.newFixedThreadPool(10);
 		for (String webPageName : fileNames) {
 			WebPageProcessor processor = new WebPageProcessorImpl();
-			CompletableFuture.supplyAsync(() -> {
-				return ParseUtils.parseFileToRetrievePages(webPageName);
-			}, executorService)
+			CompletableFuture.supplyAsync(() ->ParseUtils.parseFileToRetrievePages(webPageName)
+			, executorService)
 			.thenApplyAsync(res -> processor.crawlWebPages(webPageName,res))
 			.thenAcceptAsync(res -> processor.printCrawlingResults(res));
 		}
